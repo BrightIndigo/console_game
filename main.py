f2b239078fbd096bf2094f1c80058f4d7bc46c2f
BrightIndigo <128691376+BrightIndigo@users.noreply.github.com>
2023-10-13 19:16:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class Player:
     def __init__(self):
         self.zycie = 100
         self.pancerz = 0
-        self.atak = 5
+        self.atak = 15
         self.pieniadze = 10
         self.rodzaj_obr = "miażdżone"
         self.przedmioty = {}
@@ -23,7 +23,12 @@ class Player:
     def wykonaj_atak(self, enemy):
         if enemy.zycie >= 1:
             wytrzymalosc_przeciwnika = enemy.zycie + enemy.pancerz
-            wytrzymalosc_przeciwnika -= self.atak
+            damage = self.atak
+            if "nóż" in self.przedmioty:
+                damage *= self.przedmioty["nóż"]
+            wytrzymalosc_przeciwnika -= damage
+            enemy.zycie = max(0, wytrzymalosc_przeciwnika)
+
 
 class Enemy:
     def __init__(self):
@@ -32,7 +37,7 @@ class Enemy:
         self.atak = 5
         self.rodzaj_obr = "kłute"
         self.przedmioty = {
-            "nóż": "anty-pancerz"
+            "nóż": 2
         }
 
     def statystyki(self):
@@ -49,12 +54,39 @@ class Enemy:
     def wykonaj_atak(self, player):
         if player.zycie >= 1:
             wytrzymalosc_gracza = player.zycie + player.pancerz
-            wytrzymalosc_gracza -= self.atak
+            damage = self.atak
+            if "nóż" in self.przedmioty:
+                damage *= self.przedmioty["nóż"]
+            wytrzymalosc_gracza -= damage
+            player.zycie = max(0, wytrzymalosc_gracza)
 
-# Create instances of the Player and Enemy
+
 Gracz = Player()
 Przeciwnik = Enemy()
 
 while True:
     Gracz.statystyki()
     Przeciwnik.statystyki()
+
+    decyzja_przeciwnika = random.randint(1, 2)
+
+    if decyzja_przeciwnika == 1:
+        Przeciwnik.wykonaj_atak(Gracz)
+    elif decyzja_przeciwnika == 2:
+        Przeciwnik.leczenie()
+
+    decyzja_gracza = input("Wykonaj ruch: (1 - atak, 2 - leczenie: )")
+
+    if decyzja_gracza == "1":
+        Gracz.wykonaj_atak(Przeciwnik)
+    elif decyzja_gracza == "2":
+        Gracz.leczenie()
+
+    if Gracz.zycie <= 0:
+        print("Przegrałeś!")
+        break
+    elif Przeciwnik.zycie <= 0:
+        print("Wygrałeś!")
+        break
+
+print("Koniec gry!")
