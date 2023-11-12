3c711eae26b43da78d9bbb1a3a93da3d9e0c5401
BrightIndigo <128691376+BrightIndigo@users.noreply.github.com>
2023-11-12 15:29:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Player:
         self.zycie = 100
         self.pancerz = 0
         self.atak = 15
-        self.pieniadze = 10
+        self.złoto = 10
         self.rodzaj_obr = "miażdżone"
         self.przedmioty = {}
         self.energia = 3
@@ -13,7 +13,7 @@ class Player:
 
     def statystyki(self):
         print(f"Statystyki gracza: \nŻycie: {self.zycie}, Pancerz: {self.pancerz}, Atak: {self.atak}, Pieniądze: "
-              f"{self.pieniadze}, Rodzaj obrażeń: {self.rodzaj_obr}, Przedmioty: {self.przedmioty}, Energia: "
+              f"{self.złoto}, Rodzaj obrażeń: {self.rodzaj_obr}, Przedmioty: {self.przedmioty}, Energia: "
               f"{self.energia}, Punkty doświadczenia: {self.pd}")
 
     def leczenie(self):
@@ -52,6 +52,7 @@ class Player:
 
 class Enemy:
     def __init__(self):
+        self.poziom = 1
         self.zycie = 50
         self.pancerz = 1
         self.atak = 5
@@ -80,11 +81,17 @@ class Enemy:
                 damage *= self.przedmioty["nóż"]
             wytrzymalosc_gracza -= damage
             player.zycie = max(0, wytrzymalosc_gracza)
+    def dodaj_poziom(self):
+        self.zycie += 70
+        self.pancerz += 1
+        self.atak += 2
+
 
 Gracz = Player()
 Przeciwnik = Enemy()
 def bitwa():
-    while True:
+    bitwa = True
+    while bitwa:
         Gracz.statystyki()
         Przeciwnik.statystyki()
 
@@ -104,16 +111,17 @@ def bitwa():
         elif decyzja_gracza == "3":
             Gracz.umiejetnosc(Przeciwnik)
 
-
         if Gracz.zycie <= 0:
             print("Przegrałeś!")
-            break
+            bitwa = False
         elif Przeciwnik.zycie <= 0:
+            zwyciestwo = True
             print("Wygrałeś!")
             Gracz.pd += 10
-            break
-
+            Przeciwnik.poziom += 1
+            bitwa = False
     print("Koniec bitwy!")
+    return zwyciestwo
 
 def eksploracja():
     lokacje = {1: "jaskinie",
@@ -132,7 +140,7 @@ def eksploracja():
             while jaskinia == True:
                 decyzja = input("Wejdź - 1, Omiń - 2, Szukaj innej drogi - 3, Poczekaj - 4, Zobacz statystyki - 5: ")
                 if decyzja == "1":
-                    print("Znajdujesz ghoula lv1, zaczyna się bitwa...")
+                    print(f"Znajdujesz ghoula poziom {Przeciwnik.poziom}, zaczyna się bitwa...")
                     bitwa()
                     jaskinia = False
                 elif decyzja == "2":
@@ -154,11 +162,33 @@ def eksploracja():
                         print(f"{przypadki[1]} Tracisz energię.")
                         jaskinia = False
                     elif losowy_przypadek == 2:
-                        print(f"{przypadki[2]}")
+                        Gracz.zycie -= 20
+                        print(f"{przypadki[2]} tracisz 20 pkt zdrowia")
                     elif losowy_przypadek == 3:
-                        print(f"{przypadki[3]}")
+                        przedmioty = ["nóż", "mikstura lecznicza", "zbroja", "hełm", "miecz"]
+                        przedmiot = random.choice(przedmioty)
+                        dl = len(Gracz.przedmioty)
+                        Gracz.przedmioty[dl] = przedmiot
+                        print(f"{przypadki[3]}, {przedmiot}")
                     elif losowy_przypadek == 4:
                         print(f"{przypadki[4]}")
+                        print("Włóczęga: oddawaj całe swoje złoto, albo cię zabiję")
+                        odpowiedz = input("Ty: (a) Po moim trupie, (b) Oto moje złoto, (c) (uciekasz)")
+                        if odpowiedz == 'a':
+                            b = bitwa()
+                            if b == True:
+                                Gracz.złoto += 10
+                                print("Włóczęga miał ze sobą 10 szt. złota")
+                        elif odpowiedz == 'b':
+                            Gracz.złoto = 0
+                            print("Oddajesz całe złoto")
+                        elif odpowiedz == 'c':
+                            a = random.randint(1, 2)
+                            if a == 1:
+                                print("Udało ci się zbiec")
+                            elif a == 2:
+                                print("Nie udało ci się zbiec, włóczęga atakuje")
+                                bitwa()
 
                 elif decyzja == "4":
                     print("Czekasz...")
@@ -166,15 +196,19 @@ def eksploracja():
                     print("Odpoczywasz, zyskujesz energię, ale tracisz pd...")
                 elif decyzja == "5":
                     Gracz.statystyki()
-
+#lokacja 2 -----------------------------------------------------------------------------------------------
         elif losowa_lokacja == 2:
             print(f"Podczas swojej tułaczki znajdujesz {lokacje[losowa_lokacja]}")
+# lokacja 3 -----------------------------------------------------------------------------------------------
         elif losowa_lokacja == 3:
             print(f"Podczas swojej tułaczki znajdujesz {lokacje[losowa_lokacja]}")
+# lokacja 4 -----------------------------------------------------------------------------------------------
         elif losowa_lokacja == 4:
             print(f"Podczas swojej tułaczki znajdujesz {lokacje[losowa_lokacja]}")
+# lokacja 5 -----------------------------------------------------------------------------------------------
         elif losowa_lokacja == 5:
             print(f"Podczas swojej tułaczki znajdujesz {lokacje[losowa_lokacja]}")
+# lokacja 6 -----------------------------------------------------------------------------------------------
         elif losowa_lokacja == 6:
             print(f"Podczas swojej tułaczki znajdujesz {lokacje[losowa_lokacja]}")
 
